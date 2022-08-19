f365f22203bc7ec2e850ad194762eaf0a74d6f3e
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-08-19 10:53:35 +0800

    modified prime.js

--- src/prime.js
+++ src/prime.js
@@ -1,17 +1,31 @@
 import _ from 'lodash';
-import readlineSync from 'readline-sync';
+import { game } from './index.js';
 
-const question = (text) => readlineSync.question(text);
+const answer = (number) => {
+  let result = 0;
+  let count = 2;
+
+  while (count <= number) {
+    if (number % count === 0) {
+      result += count;
+    }
+    count += 1;
+  }
+  const isPrime = result === number ? 'yes' : 'no';
+  return isPrime;
+};
 
 const brainPrime = (userName) => {
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
     const number = _.random(1, 100);
-    console.log(`Question: ${number}`);
-    question('Your answer: ');
-    console.log('Correct!');
+
+    if (game(userName, number, answer(number)) === false) {
+      break;
+    } else if (i === 2) {
+      console.log(`Congratulations, ${userName}!`);
+    }
   }
-  console.log(`Congratulations, ${userName}!`);
 };
 
 export default brainPrime;
