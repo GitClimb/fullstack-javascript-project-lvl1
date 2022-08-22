6624c9a34123d608b3cbb3604429ba7d6acbe28c
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-08-22 18:55:21 +0800

    add gretting

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const grettingUser = () => {
+  console.log('Welcome to the Brain Games!');
   const getUserName = readlineSync.question('May I have your name? ');
   return `Hello, ${getUserName}!`;
 };
