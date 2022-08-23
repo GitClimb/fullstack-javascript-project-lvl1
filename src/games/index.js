e707d284da92d2443766e3fc63ec9440a16d6a97
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-08-23 19:50:47 +0800

    modified runGameEngine

--- src/games/index.js
+++ src/games/index.js
@@ -7,17 +7,23 @@ const runGameEngine = (descriptString, generateRound) => {
   const getUserName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${getUserName}!`);
   console.log(descriptString);
+  let count = 0;
 
   for (let i = 0; i < rounds; i += 1) {
-    const [question, answer] = generateRound;
-    if (question === answer) {
+    console.log(`Question: ${generateRound[0]}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === generateRound[1]) {
       console.log('Correct!');
+      count += 1;
     } else {
       console.log(`'${question}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${getUserName}!`);
       break;
     }
   }
-  console.log(`Congratulations, ${getUserName}!`);
+  if (count === 3) {
+    console.log(`Congratulations, ${getUserName}!`);
+  }
 };
 
 export default runGameEngine;
