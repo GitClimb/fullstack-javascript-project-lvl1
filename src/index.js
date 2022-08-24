49743576c7a9c8cd2d9ba78b31ee7de6febee206
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-08-24 09:11:43 +0800

    fix index.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+import readlineSync from 'readline-sync';
+
+const rounds = 3;
+
+const runGameEngine = (rules, generateRound) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(descriptString);
+
+  for (let i = 0; i < rounds; i += 1) {
+    const [question, answer] = generateRound();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+  
+};
+
+export default runGameEngine;
