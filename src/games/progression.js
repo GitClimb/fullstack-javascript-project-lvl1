0def045b838cf87207260d9ced0ea336cb1e9a30
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-08-26 20:11:14 +0800

    fix length coll in brain-progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,7 +7,7 @@ const getQuestion = (length, start, steps) => {
   const lengthProgression = length;
   const coll = [start];
 
-  for (let i = 0; i < lengthProgression; i += 1) {
+  for (let i = 0; i <= lengthProgression; i += 1) {
     coll.push(coll[coll.length - 1] + steps);
   }
   const answer = coll[_.random(0, coll.length - 1)];
@@ -16,7 +16,7 @@ const getQuestion = (length, start, steps) => {
 };
 
 export const generateRound = () => {
-  const length = _.random(5, 10);
+  const length = _.random(5, 9);
   const start = _.random(1, 100);
   const steps = _.random(1, 100);
 
