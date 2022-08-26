9fc1c7a46d806827e19f20d360353d323dd8ff48
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-08-26 18:04:36 +0800

    modified brain-calc

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import { descriptionGame, generateRound } from '../../src/games/calc.js';
-import runGameEngine from '../../src/index.js';
+import runCalcGame from '../../src/games/calc.js';
 
-runGameEngine(descriptionGame, generateRound);
+runCalcGame();
--- src/games/calc.js
+++ src/games/calc.js
@@ -1,26 +1,28 @@
 import _ from 'lodash';
+import runGameEngine from '../index.js';
 
-export const descriptionGame = 'What is the result of the expression?';
+const descriptionGame = 'What is the result of the expression?';
 
-const mathSymb = ['+', '-', '*'];
+const operators = ['+', '-', '*'];
 
-const answer = (num1, num2, symbol) => {
-  let result = 0;
-
-  if (symbol === '+') {
-    result = num1 + num2;
-  } else if (symbol === '-') {
-    result = num1 - num2;
-  } else {
-    result = num1 * num2;
+const getAnswer = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      return String(num1 + num2);
+    case '-':
+      return String(num1 - num2);
+    default:
+      return String(num1 * num2);
   }
-  return String(result);
 };
 
-export const generateRound = () => {
+const generateRound = () => {
   const number1 = _.random(1, 100);
   const number2 = _.random(1, 100);
-  const symbol = mathSymb.at(_.random(0, 2));
-  const expression = `${number1} ${symbol} ${number2}`;
-  return [expression, answer(number1, number2, symbol)];
+  const randomOperator = operators.at(_.random(0, 2));
+  const question = `${number1} ${randomOperator} ${number2}`;
+  const answer = getAnswer(number1, number2, randomOperator);
+  return [question, answer];
 };
+
+export default () => runGameEngine(descriptionGame, generateRound);
