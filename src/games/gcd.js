84b39d57660938300d83b7f3fa7136b236528ed5
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-08-29 19:25:51 +0800

    modified brain-gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -7,14 +7,14 @@ const getGcd = (num1, num2) => {
   if (num1 === 0) {
     return num2;
   }
-  return String(getGcd(num2 % num1, num1));
+  return getGcd(num2 % num1, num1);
 };
 
 const generateRound = () => {
   const number1 = _.random(1, 100);
   const number2 = _.random(1, 100);
   const question = `${number1} ${number2}`;
-  const answer = getGcd(number1, number2);
+  const answer = String(getGcd(number1, number2));
   return [question, answer];
 };
 
