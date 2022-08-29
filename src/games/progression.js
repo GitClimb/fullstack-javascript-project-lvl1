5695a89541ce3714ffe039c314e6fccd496e3bea
Ilya Resnyansky <resnyansky.i.o@gmail.com>
2022-08-29 19:21:34 +0800

    modified brain-progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,24 +3,25 @@ import runGameEngine from '../index.js';
 
 export const descriptionGame = 'What number is missing in the progression?';
 
-const getQuestion = (length, start, steps) => {
+const generateProgression = (length, start, steps) => {
   const lengthProgression = length;
   const coll = [start];
 
   for (let i = 0; i <= lengthProgression; i += 1) {
     coll.push(coll[coll.length - 1] + steps);
   }
-  const answer = coll[_.random(0, coll.length - 1)];
-  coll[coll.indexOf(answer)] = '..';
-  return [coll.join(' '), String(answer)];
+  return coll;
 };
 
 export const generateRound = () => {
   const length = _.random(5, 9);
   const start = _.random(1, 100);
   const steps = _.random(1, 100);
-
-  const [question, answer] = getQuestion(length, start, steps);
+  const progression = generateProgression(length, start, steps);
+  const randomValue = progression[_.random(0, progression.length - 1)];
+  progression[progression.indexOf(randomValue)] = '..';
+  const answer = String(randomValue);
+  const question = progression.join(' ');
   return [question, answer];
 };
 
